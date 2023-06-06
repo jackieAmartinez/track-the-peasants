1a84dfbdabd251842502b13975ab6a8a5cdd940f
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-06 15:05:47 -0400

    schema updated

--- db/schema.sql
+++ db/schema.sql
@@ -33,3 +33,39 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
 );
+
+-- Create the databases used for storing data 
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Table for the department i.e.
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
+);
+
+-- Table for the preset role that employee will fill
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
+
+
+-- Table for individual employee info
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
+);
