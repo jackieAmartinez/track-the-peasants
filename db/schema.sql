ef63f3bc4e79863b7cbd8a7cc95e3105cd426b99
Chester Springwood <jackieAmartinez@gmail.com>
2023-06-10 19:50:35 -0400

    seeds.sql/schema.sql coordinated

--- db/schema.sql
+++ db/schema.sql
@@ -1,71 +1,37 @@
 -- Creates databases
-DROP DATABASE IF EXISTS emp_db;
-CREATE DATABASE emp_db;
-
-USE emp_db;
-
--- Table for dept
-CREATE TABLE dept (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    dept_area VARCHAR(30) NOT NULL
-);
-
--- Table for job titles
-CREATE TABLE role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL, 
-    dept_id INT NOT NULL,
-    
-    FOREIGN KEY (dept_id)
-    REFERENCES dept(id)
-);
-
--- Table with individual emp info
-CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
-    manager_id INT,
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-);
-
--- Create the databases used for storing data 
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- Table for the department i.e.
-CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+-- Table for dept
+CREATE TABLE dept (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    deptName VARCHAR(30) NOT NULL
 );
 
--- Table for the preset role that employee will fill
-CREATE TABLE role (
+-- Table for job titles
+CREATE TABLE position (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id) 
+    salary DECIMAL, 
+    deptartmentId INT NOT NULL,
+    
+    FOREIGN KEY (deptartmentId)
     REFERENCES department(id)
 );
 
-
--- Table for individual employee info
+-- Table with individual emp info
 CREATE TABLE employee (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id) 
-    REFERENCES role(id),
-    manager_id INT,
-    FOREIGN KEY (manager_id) 
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    positionId INT NOT NULL,
+    FOREIGN KEY (positionId)
+    REFERENCES position(id),
+    managerId INT,
+    FOREIGN KEY (managerId)
     REFERENCES employee(id)
 );
+
+
